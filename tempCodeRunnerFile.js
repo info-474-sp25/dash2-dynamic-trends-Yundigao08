163c68603c65e23bdc86adbae2d59e314c3902eb
Yundi Gao <ygao27@uw.edu>
2025-05-16 00:21:16 -0700

    Change title and insert csv file

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,39 @@
+d3.csv("weather.csv").then(data => {
+    console.log(data);
+    // 2.b: ... AND TRANSFORM DATA
+
+    // 3.a: SET SCALES FOR CHART 1
+
+
+    // 4.a: PLOT DATA FOR CHART 1
+
+
+    // 5.a: ADD AXES FOR CHART 1
+
+
+    // 6.a: ADD LABELS FOR CHART 1
+
+
+    // 7.a: ADD INTERACTIVITY FOR CHART 1
+    
+
+    // ==========================================
+    //         CHART 2 (if applicable)
+    // ==========================================
+
+    // 3.b: SET SCALES FOR CHART 2
+
+
+    // 4.b: PLOT DATA FOR CHART 2
+
+
+    // 5.b: ADD AXES FOR CHART 
+
+
+    // 6.b: ADD LABELS FOR CHART 2
+
+
+    // 7.b: ADD INTERACTIVITY FOR CHART 2
+
+
+});
